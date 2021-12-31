58823ccf578c4cfcfc0e60d10a516cce953f19ca
NikitaShkril <Shkril95@yandex.ru>
2021-12-31 14:35:09 +0300

    3

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,59 +3,52 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-
+        //1st
         int clientIos = 0;
         if (clientIos > 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
+            System.out.println("Установите версию приложения для IOS по ссылке");
         } else {
             if (clientIos < 1) {
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
         }
+
         //2nd
-        int clientOS = 0;
-        int clientDeviceYear = 2014;
-        if (clientOS == 1 && clientDeviceYear >= 2015) {
+        int clientOs = 1;
+        int clientDeviceYear = 2015;
+        boolean isLiteNeeded = (clientDeviceYear < 2015);
+        if (clientOs == 1 && isLiteNeeded) {
+            System.out.println("Установите облегченную версию приложения для Ios по ссылке");
+        } else if (clientOs == 1) {
             System.out.println("Установите версию приложения для IOS по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear <= 2015) {
-            System.out.println("Установите упрощенную версию приложения для IOS по ссылке");
-        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
-            System.out.println("Установите приложение для Android по ссылке");
-        } else if (clientOS == 0 && clientDeviceYear <= 2015) {
-            System.out.println("Установите упрощенную версию приложения для Android по ссылке");
+        } else if (clientOs == 0 && isLiteNeeded) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOs == 0) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
 
         //3rd
-        int year = 2030;
-        switch (year) {
-            case 2016:
-                System.out.println("Високосный год");
-                break;
-            case 2017:
-            case 2018:
-            case 2019:
-            case 2020:
-                System.out.println("Обычный год");
-                break;
-            default:
-                System.out.println("На столько календарь не рассчитан");
+        int year = 2015;
+        boolean isLeapYear = ((year % 4 == 0 || year % 400 == 0) && (year % 100 != 0));
+        if (isLeapYear) {
+            System.out.println("Год високосный ");
+        } else {
+            System.out.println("Год не високосный");
         }
-        //4th Переделал
 
-        int deliveryDistance = 95;
-        int deliveryDaysUnder20Km = 1;
-        int deliveryDaysOver20Km = deliveryDaysUnder20Km + 1;
-        int deliveryDaysOver60Km = deliveryDaysOver20Km + 1;
-        if (deliveryDistance == 20) {
-            System.out.println("Потребуется дней " +deliveryDaysUnder20Km);
-        } else if (deliveryDistance >= 20 && deliveryDistance <= 60) {
-            System.out.println("Потребуется дней " +deliveryDaysOver20Km);
-        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней " +deliveryDaysOver60Km);
+        //4th
+        int deliveryDistance = 61;
+        int deliveryDays = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("Понадобится дней " + deliveryDays);
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Понадобится дней " + (deliveryDays + 1));
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Понадобится дней " + (deliveryDays + 2));
         }
 
         //5th
-        int monthNubmer = 13;
+        int monthNubmer = 12;
         switch (monthNubmer) {
             case 1:
             case 2:
